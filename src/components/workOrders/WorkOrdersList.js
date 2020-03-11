bfd2c2716edf674627da4ec0ec30909af021358f
Yatiac <master.152@gmail.com>
2020-03-10 22:38:58 -0500

    Add Search functionality with Highlight

--- src/components/workOrders/WorkOrdersList.js
+++ src/components/workOrders/WorkOrdersList.js
@@ -6,6 +6,7 @@ import { compose } from "redux";
 import { Redirect, useHistory } from "react-router-dom";
 import moment from "moment";
 import "moment/locale/es";
+import Highlight from 'react-highlighter';
 
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
@@ -16,37 +17,26 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import Topbar from "../layouts/Topbar";
-
+import StatusBadge from "./StatusBadge";
 
 class WorkOrdersList extends Component {
+  state = {
+    searchTerm: ""
+  };
   handleClick = e => {
     const id = e.target.getAttribute("data-id");
     this.props.history.push(`/workOrder/${id}`);
   };
 
-  statusBadge = (status, id) => {
-    let color = "";
-    switch (status) {
-      case "Nueva":
-        color = "red";
-        break;
-      case "En Proceso":
-        color = "blue";
-        break;
-      case "Entregado":
-        color = "green";
-        break;
-      default:
-        color = "yellow";
-        break;
-    }
-    return (
-      <span
-        className={`new badge ${color}`}
-        data-badge-caption={status}
-        data-id={id}
-      ></span>
-    );
+  handleSubmit = e => {
+    e.preventDefault();
+    alert("busqueda");
+  };
+
+  handleChange = e => {
+    this.setState({
+      searchTerm: e.target.value
+    });
   };
 
   render() {
@@ -98,7 +88,10 @@ class WorkOrdersList extends Component {
                         ))}
                       </TableCell>
                       <TableCell data-id={workOrder.id}>
-                        {this.statusBadge(workOrder.status, workOrder.id)}
+                        <StatusBadge
+                          status={workOrder.status}
+                          id={workOrder.id}
+                        />
                       </TableCell>
                     </TableRow>
                   ))}
@@ -118,43 +111,84 @@ class WorkOrdersList extends Component {
             </div>
           </div>
         </div>
+
+        {/* *********** */}
+        {/* MOBILE VIEW */}
+        {/* *********** */}
         <div className="hide-on-med-and-up white">
           <Topbar title="Órdenes de Trabajo" />
           <p> </p>
           <ul className="collection no-margin">
+            <li>
+              <form style={{ padding: "0" }} onSubmit={this.handleSubmit}>
+                <div className="input-field col s6">
+                  <i className="material-icons prefix">search</i>
+                  <input
+                    id="icon_prefix"
+                    type="text"
+                    className="validate"
+                    onChange={this.handleChange}
+                    value={this.state.searchTerm}
+                    autoComplete="off"
+                  />
+                  <label htmlFor="icon_prefix">Buscar por Placa o Nombre</label>
+                </div>
+              </form>
+            </li>
             {workOrders &&
-              workOrders.map((workOrder, index) => (
-                <li
-                  className="collection-item"
-                  key={index}
-                  data-id={workOrder.id}
-                  onClick={this.handleClick}
-                >
-                  <div data-id={workOrder.id}>
-                    <span className="title" data-id={workOrder.id}>
-                      <strong data-id={workOrder.id}>
-                        {workOrder.owner.name}
-                      </strong>{" "}
-                    </span>
-                    <p className="no-margin" data-id={workOrder.id}></p>
-                    <span className="secondary-content" data-id={workOrder.id}>
-                      {" "}
-                      {this.statusBadge(workOrder.status, workOrder.id)}
-                    </span>
-                    <p className="no-margin" data-id={workOrder.id}></p>
-                    <span data-id={workOrder.id}>
-                      {`[${workOrder.vehicle.plate}] ${workOrder.vehicle.maker} ${workOrder.vehicle.model} ${workOrder.vehicle.color} (${workOrder.vehicle.year})`}
-                    </span>
-                    <p className="no-margin" data-id={workOrder.id}></p>
-                    <span
-                      className="grey-text no-margin"
-                      data-id={workOrder.id}
-                    >
-                      {moment(workOrder.date).format("MMMM D YYYY")}
-                    </span>
-                  </div>
-                </li>
-              ))}
+              workOrders
+                .filter(e => {
+                  if (this.state.searchTerm.length > 0) {
+                    const term = this.state.searchTerm.toUpperCase();
+                    return (
+                      e.vehicle.plate.toUpperCase().includes(term) ||
+                      e.owner.name.toUpperCase().includes(term)
+                    );
+                  }
+                  return true;
+                })
+                .map((workOrder, index) => (
+                  <li
+                    className="collection-item"
+                    key={index}
+                    data-id={workOrder.id}
+                    onClick={this.handleClick}
+                  >
+                    <div data-id={workOrder.id}>
+                      <span className="title" data-id={workOrder.id}>
+                        <strong data-id={workOrder.id}>                          
+                          {/* {workOrder.owner.name.replace(
+                            new RegExp(this.state.searchTerm.toUpperCase(), "gi"),
+                            match => <mark>${match}</mark>
+                          )} */}
+                          <Highlight search={this.state.searchTerm}>{workOrder.owner.name}</Highlight>
+                        </strong>{" "}
+                      </span>
+                      <p className="no-margin" data-id={workOrder.id}></p>
+                      <span
+                        className="secondary-content"
+                        data-id={workOrder.id}
+                      >
+                        {" "}
+                        <StatusBadge
+                          status={workOrder.status}
+                          id={workOrder.id}
+                        />
+                      </span>
+                      <p className="no-margin" data-id={workOrder.id}></p>
+                      <span data-id={workOrder.id}>
+                       [<Highlight search={this.state.searchTerm}>{workOrder.vehicle.plate}</Highlight>] {`${workOrder.vehicle.maker} ${workOrder.vehicle.model} ${workOrder.vehicle.color} (${workOrder.vehicle.year})`}
+                      </span>
+                      <p className="no-margin" data-id={workOrder.id}></p>
+                      <span
+                        className="grey-text no-margin"
+                        data-id={workOrder.id}
+                      >
+                        {moment(workOrder.date).format("MMMM D YYYY")}
+                      </span>
+                    </div>
+                  </li>
+                ))}
           </ul>
           <div className="fixed-action-btn" style={{ marginBottom: "5rem" }}>
             <Link to="/createWorkOrder">
@@ -170,7 +204,8 @@ class WorkOrdersList extends Component {
 }
 const mapStateToProps = state => {
   return {
-    workOrders: state.firestore.ordered.workOrders
+    workOrders: state.firestore.ordered.workOrders,
+    searchTerm: ""
   };
 };
 export default compose(
